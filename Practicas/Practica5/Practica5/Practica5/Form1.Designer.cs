a1928381910de7694c7a53d003700dd7144b05f2
Federico 'Magus' Rocca <frocca17@gmail.com>
2019-08-17 20:26:05 -0300

    - Practica 5, botones, textboxes y manejo de eventos de input

---
+++ Practicas/Practica5/Practica5/Practica5/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Practica5
+{
+    partial class Practica5
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvlNombre = new System.Windows.Forms.Label();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txbApellido = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txbEdad = new System.Windows.Forms.TextBox();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.txbDireccion = new System.Windows.Forms.TextBox();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.rtbResultado = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lvlNombre
+            // 
+            this.lvlNombre.AutoSize = true;
+            this.lvlNombre.Location = new System.Drawing.Point(12, 9);
+            this.lvlNombre.Name = "lvlNombre";
+            this.lvlNombre.Size = new System.Drawing.Size(44, 13);
+            this.lvlNombre.TabIndex = 0;
+            this.lvlNombre.Text = "Nombre";
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Location = new System.Drawing.Point(70, 6);
+            this.txbNombre.MaxLength = 36;
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(229, 20);
+            this.txbNombre.TabIndex = 1;
+            this.txbNombre.TextChanged += new System.EventHandler(this.txbNombre_TextChanged);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(207, 227);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txbApellido
+            // 
+            this.txbApellido.Location = new System.Drawing.Point(70, 35);
+            this.txbApellido.MaxLength = 36;
+            this.txbApellido.Name = "txbApellido";
+            this.txbApellido.Size = new System.Drawing.Size(229, 20);
+            this.txbApellido.TabIndex = 2;
+            this.txbApellido.TextChanged += new System.EventHandler(this.txbApellido_TextChanged);
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(12, 35);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 3;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // txbEdad
+            // 
+            this.txbEdad.Location = new System.Drawing.Point(70, 61);
+            this.txbEdad.MaxLength = 2;
+            this.txbEdad.Name = "txbEdad";
+            this.txbEdad.Size = new System.Drawing.Size(30, 20);
+            this.txbEdad.TabIndex = 3;
+            this.txbEdad.TextChanged += new System.EventHandler(this.txbEdad_TextChanged);
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(12, 61);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(32, 13);
+            this.lblEdad.TabIndex = 5;
+            this.lblEdad.Text = "Edad";
+            // 
+            // txbDireccion
+            // 
+            this.txbDireccion.Location = new System.Drawing.Point(70, 84);
+            this.txbDireccion.MaxLength = 36;
+            this.txbDireccion.Name = "txbDireccion";
+            this.txbDireccion.Size = new System.Drawing.Size(229, 20);
+            this.txbDireccion.TabIndex = 4;
+            this.txbDireccion.TextChanged += new System.EventHandler(this.txbDireccion_TextChanged);
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(12, 87);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(52, 13);
+            this.lblDireccion.TabIndex = 7;
+            this.lblDireccion.Text = "Dirección";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 113);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(12, 227);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 5;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // rtbResultado
+            // 
+            this.rtbResultado.Location = new System.Drawing.Point(70, 129);
+            this.rtbResultado.Name = "rtbResultado";
+            this.rtbResultado.ReadOnly = true;
+            this.rtbResultado.Size = new System.Drawing.Size(229, 92);
+            this.rtbResultado.TabIndex = 99;
+            this.rtbResultado.Text = "";
+            // 
+            // Practica5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 256);
+            this.Controls.Add(this.rtbResultado);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txbDireccion);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.txbEdad);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.txbApellido);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txbNombre);
+            this.Controls.Add(this.lvlNombre);
+            this.Name = "Practica5";
+            this.Text = "Practica 5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lvlNombre;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txbApellido;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txbEdad;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.TextBox txbDireccion;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.RichTextBox rtbResultado;
+    }
+}
+
---
+++ Practicas/Practica5/Practica5/Practica5/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica5
+{
+    public partial class Practica5 : Form
+    {
+        public Practica5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+            bool isFormCorrect = true;
+
+            if( string.IsNullOrWhiteSpace(txbNombre.Text) )
+            {
+                txbNombre.BackColor = Color.Red;
+                isFormCorrect = false;
+            }
+            else
+            {
+                txbNombre.BackColor = Color.White;
+            }
+
+            if (string.IsNullOrWhiteSpace(txbApellido.Text))
+            {
+                txbApellido.BackColor = Color.Red;
+                isFormCorrect = false;
+            }
+            else
+            {
+                txbApellido.BackColor = Color.White;
+            }
+
+            if (string.IsNullOrWhiteSpace(txbEdad.Text))
+            {
+                txbEdad.BackColor = Color.Red;
+                isFormCorrect = false;
+            }
+            else
+            {
+                txbEdad.BackColor = Color.White;
+            }
+
+            if (string.IsNullOrWhiteSpace(txbDireccion.Text))
+            {
+                txbDireccion.BackColor = Color.Red;
+                isFormCorrect = false;
+            }
+            else
+            {
+                txbDireccion.BackColor = Color.White;
+            }
+
+            if( isFormCorrect )
+            {
+                rtbResultado.Text = "Apellido y nombre: " + txbApellido.Text + " " + txbNombre.Text;
+                rtbResultado.Text += Environment.NewLine;
+                rtbResultado.Text += "Edad: " + txbEdad.Text;
+                rtbResultado.Text += Environment.NewLine;
+                rtbResultado.Text += "Dirección: " + txbDireccion.Text;
+            }
+            else
+            {
+                rtbResultado.Text = "";
+            }
+
+        }
+
+        private void txbEdad_TextChanged(object sender, EventArgs e)
+        {
+            if( !txbEdad.Text.All(char.IsDigit) )
+            {
+                txbEdad.Text = txbEdad.Text.Substring(0, txbEdad.Text.Length - 1);
+                txbEdad.SelectionStart = txbEdad.Text.Length;
+            }
+        }
+
+        private void txbNombre_TextChanged(object sender, EventArgs e)
+        {
+            txbNombre.Text = txbNombre.Text.ToUpper();
+            txbNombre.SelectionStart = txbNombre.Text.Length;
+        }
+
+        private void txbApellido_TextChanged(object sender, EventArgs e)
+        {
+            txbApellido.Text = txbApellido.Text.ToUpper();
+            txbApellido.SelectionStart = txbApellido.Text.Length;
+        }
+
+        private void txbDireccion_TextChanged(object sender, EventArgs e)
+        {
+            txbDireccion.Text = txbDireccion.Text.ToUpper();
+            txbDireccion.SelectionStart = txbDireccion.Text.Length;
+        }
+    }
+}
